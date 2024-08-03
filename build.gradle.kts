e2f4be856bbc7df1f67e658615908e1033e77578
Hung <hungtrinh2801@gmail.com>
2024-08-03 21:52:16 +0700

    Revert "create code stream"
    This reverts commit 066651f7430c892bd595494140f0308a6d848e44.

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,11 +79,6 @@ subprojects {
     implementation(kotlin("stdlib")) // adds standard kotlin features, like listOf, mapOf etc
     implementation("com.github.Blatzar:NiceHttp:0.4.11") // http library
     implementation("org.jsoup:jsoup:1.17.2") // html parser
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
-    implementation("io.karn:khttp-android:0.1.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("com.faendir.rhino:rhino-android:1.6.0")
   }
 }
 
